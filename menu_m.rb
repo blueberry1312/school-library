df759c1c81de2b30287664539f52bbe250c44e01
cabraham1 <christopherabraham8@gmail.com>
2023-05-23 16:27:07 +0100

    Creat menu method file

---
+++ menu_m.rb
@@ -0,0 +1,43 @@
+  # Menu methods
+  def display_menu
+    menu = {
+      1 => 'List all books',
+      2 => 'List all people',
+      3 => 'Create a person',
+      4 => 'Create a book',
+      5 => 'Create a rental',
+      6 => 'List all rentals for a given person id',
+      7 => 'Exit'
+    }
+
+    puts "\nSelect an option by entering a number:"
+
+    menu.each do |key, value|
+      puts "#{key} - #{value}"
+    end
+
+    validate_option
+  end
+
+  def validate_option
+    option = gets.chomp.to_i
+
+    menu_actions = {
+      1 => :list_all_books,
+      2 => :list_all_people,
+      3 => :create_person,
+      4 => :create_book,
+      5 => :create_rental,
+      6 => :list_all_rentals,
+      7 => :exit
+    }
+
+    action = menu_actions[option]
+
+    if action
+      send(action)
+    else
+      puts 'Select a valid option'
+      display_menu
+    end
+  end
