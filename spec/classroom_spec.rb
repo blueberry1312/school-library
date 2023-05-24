8c0dd1b78c3877411dd8fa9b74d5ce2cabdc7ed0
cabraham1 <christopherabraham8@gmail.com>
2023-05-24 18:44:36 +0100

    Add intance of classroom

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -5,6 +5,12 @@ describe Classroom do
     @classroom = Classroom.new('Math')
   end
 
- 
+  describe '#new' do
+    it 'takes two parameters and returns a Classroom object' do
+      expect(@classroom).to be_an_instance_of Classroom
+    end
+  end
+
+end
 
 end
