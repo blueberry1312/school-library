74a551fa88abacc66ceb6be87ee7d7851eeb81c3
cabraham1 <christopherabraham8@gmail.com>
2023-05-24 18:35:51 +0100

    Add book if title is empty

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -5,5 +5,17 @@ describe Book do
     @book = Book.new('Title', 'Author')
   end
 
-  
+  describe '#new' do
+    it 'returns a new book object' do
+      expect(@book).to be_an_instance_of Book
+    end
+  end
+
+  describe '#title' do
+    it 'returns the correct title' do
+      expect(@book.title).to eql 'Title'
+    end
+  end
+
+ 
 end
